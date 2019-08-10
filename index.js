c945ffbe247d8695836c2db896abae809971229b
Vlad Bilokur <vladimino@gmail.com>
2019-08-10 23:06:04 +0200

    Use static folder

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
